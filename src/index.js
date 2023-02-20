6a5c592f56ec760658ec3573d795be8eb5aa5518
Volodymyr Vasylkivskyi <vasilksvskij@gmail.com>
2023-02-20 13:11:07 +0200

    feat: add index.js file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,74 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  let arrExpr = [];
+  let arrDec = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    arrExpr.push(expr.slice(i, i + 10));
+  }
+  console.log(arrExpr);
+
+  for (let i = 0; i < arrExpr.length; i++) {
+    if (arrExpr[i] === '**********') {
+      arrExpr[i] = ' ';
+    }
+    arrExpr[i] = arrExpr[i].replace(/^0+/, '');
+  }
+  console.log(arrExpr);
+
+  for (let i = 0; i < arrExpr.length; i++) {
+    arrExpr[i]= arrExpr[i].replace(/11/g, '-').replace(/10/g, '.')
+  }
+  console.log(arrExpr);
+
+  for (element of arrExpr) {
+    if (element === ' ') {
+      arrDec.push(' ')
+    } else {
+      arrDec.push(MORSE_TABLE[element])
+    }
+  }
+  return arrDec.join('')
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
